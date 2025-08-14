5d61bc9acdd8eb5c762a19aaff310e964f17d24f
swaraj-tech <yadavsushma313@gmail.com>
2025-08-14 18:09:57 +0530

    Bug Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,9 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import UdyamRegistrationForm from "@/components/main";
-import { useState } from "react";
+
 
 export default function UdyamForm() {
-  const [aadhaarNumber, setAadhaarNumber] = useState('');
-  const [entrepreneurName, setEntrepreneurName] = useState('');
-  const [consentGiven, setConsentGiven] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // handle validation and OTP generation logic here
-    alert('Validate & Generate OTP clicked');
-  };
-
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Header */}
